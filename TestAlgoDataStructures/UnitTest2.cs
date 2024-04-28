800ed2b602a4f93b53321e2799e7d1d4c23b938c
rodion <rodi-98@mail.ru>
2024-04-28 10:01:01 +0300

    Явно указанные типы

--- TestAlgoDataStructures/UnitTest2.cs
+++ TestAlgoDataStructures/UnitTest2.cs
@@ -288,7 +288,7 @@ namespace AlgorithmsDataStructures2.Tests
         [TestMethod]
         public void WideAllNodesReturnsCorrectList()
         {
-            var rootNode = new BSTNode<int>(5, 5, null);
+            BSTNode<int> rootNode = new BSTNode<int>(5, 5, null);
             rootNode.LeftChild = new BSTNode<int>(3, 3, rootNode);
             rootNode.RightChild = new BSTNode<int>(8, 8, rootNode);
             rootNode.LeftChild.LeftChild = new BSTNode<int>(2, 2, rootNode.LeftChild);
@@ -296,11 +296,11 @@ namespace AlgorithmsDataStructures2.Tests
             rootNode.RightChild.LeftChild = new BSTNode<int>(7, 7, rootNode.RightChild);
             rootNode.RightChild.RightChild = new BSTNode<int>(9, 9, rootNode.RightChild);
 
-            var bst = new BST<int>(rootNode);
+            BST<int> bst = new BST<int>(rootNode);
 
-            var wideList = bst.WideAllNodes();
+            List<BSTNode<int>> wideList = bst.WideAllNodes();
 
-            var expectedKeys = new int[] { 5, 3, 8, 2, 4, 7, 9 };
+            int[] expectedKeys = new int[] { 5, 3, 8, 2, 4, 7, 9 };
             for (int i = 0; i < wideList.Count; i++)
             {
                 Assert.AreEqual(expectedKeys[i], wideList[i].NodeKey);
@@ -310,7 +310,7 @@ namespace AlgorithmsDataStructures2.Tests
         [TestMethod]
         public void DeepAllNodesInOrderReturnsCorrectList()
         {
-            var rootNode = new BSTNode<int>(5, 5, null);
+            BSTNode<int> rootNode = new BSTNode<int>(5, 5, null);
             rootNode.LeftChild = new BSTNode<int>(3, 3, rootNode);
             rootNode.RightChild = new BSTNode<int>(8, 8, rootNode);
             rootNode.LeftChild.LeftChild = new BSTNode<int>(2, 2, rootNode.LeftChild);
@@ -318,11 +318,11 @@ namespace AlgorithmsDataStructures2.Tests
             rootNode.RightChild.LeftChild = new BSTNode<int>(7, 7, rootNode.RightChild);
             rootNode.RightChild.RightChild = new BSTNode<int>(9, 9, rootNode.RightChild);
 
-            var bst = new BST<int>(rootNode);
+            BST<int> bst = new BST<int>(rootNode);
 
-            var deepList = bst.DeepAllNodes(0);
+            List<BSTNode<int>> deepList = bst.DeepAllNodes(0);
 
-            var expectedKeys = new int[] { 2, 3, 4, 5, 7, 8, 9 };
+            int[] expectedKeys = new int[] { 2, 3, 4, 5, 7, 8, 9 };
             for (int i = 0; i < deepList.Count; i++)
             {
                 Assert.AreEqual(expectedKeys[i], deepList[i].NodeKey);
@@ -332,7 +332,7 @@ namespace AlgorithmsDataStructures2.Tests
         [TestMethod]
         public void DeepAllNodesPostOrderReturnsCorrectList()
         {
-            var rootNode = new BSTNode<int>(5, 5, null);
+            BSTNode<int> rootNode = new BSTNode<int>(5, 5, null);
             rootNode.LeftChild = new BSTNode<int>(3, 3, rootNode);
             rootNode.RightChild = new BSTNode<int>(8, 8, rootNode);
             rootNode.LeftChild.LeftChild = new BSTNode<int>(2, 2, rootNode.LeftChild);
@@ -340,11 +340,11 @@ namespace AlgorithmsDataStructures2.Tests
             rootNode.RightChild.LeftChild = new BSTNode<int>(7, 7, rootNode.RightChild);
             rootNode.RightChild.RightChild = new BSTNode<int>(9, 9, rootNode.RightChild);
 
-            var bst = new BST<int>(rootNode);
+            BST<int> bst = new BST<int>(rootNode);
 
-            var deepList = bst.DeepAllNodes(1);
+            List<BSTNode<int>> deepList = bst.DeepAllNodes(1);
 
-            var expectedKeys = new int[] { 2, 4, 3, 7, 9, 8, 5 };
+            int[] expectedKeys = new int[] { 2, 4, 3, 7, 9, 8, 5 };
             for (int i = 0; i < deepList.Count; i++)
             {
                 Assert.AreEqual(expectedKeys[i], deepList[i].NodeKey);
@@ -354,7 +354,7 @@ namespace AlgorithmsDataStructures2.Tests
         [TestMethod]
         public void DeepAllNodesPreOrderReturnsCorrectList()
         {
-            var rootNode = new BSTNode<int>(5, 5, null);
+            BSTNode<int> rootNode = new BSTNode<int>(5, 5, null);
             rootNode.LeftChild = new BSTNode<int>(3, 3, rootNode);
             rootNode.RightChild = new BSTNode<int>(8, 8, rootNode);
             rootNode.LeftChild.LeftChild = new BSTNode<int>(2, 2, rootNode.LeftChild);
@@ -362,17 +362,16 @@ namespace AlgorithmsDataStructures2.Tests
             rootNode.RightChild.LeftChild = new BSTNode<int>(7, 7, rootNode.RightChild);
             rootNode.RightChild.RightChild = new BSTNode<int>(9, 9, rootNode.RightChild);
 
-            var bst = new BST<int>(rootNode);
+            BST<int> bst = new BST<int>(rootNode);
 
-            var deepList = bst.DeepAllNodes(2);
+            List<BSTNode<int>> deepList = bst.DeepAllNodes(2);
 
-            var expectedKeys = new int[] { 5, 3, 2, 4, 8, 7, 9 };
+            int[] expectedKeys = new int[] { 5, 3, 2, 4, 8, 7, 9 };
             for (int i = 0; i < deepList.Count; i++)
             {
                 Assert.AreEqual(expectedKeys[i], deepList[i].NodeKey);
             }
         }
-
 
     }
 
