4b49f6fc99c0932d682b9f8c8bdee1f175fcfcd3
rodion <rodi-98@mail.ru>
2024-05-07 06:35:06 +0300

    check

---
+++ AlgoDataStructures/BalancedBST/BalancedBST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public static class BalancedBST
+    {
+        public static int[] BBST;
+        public static int[] GenerateBBSTArray(int[] a)
+        {
+            Array.Sort(a);
+
+            BBST = new int[a.Length];
+            Balance(a, 0);
+
+            return BBST;
+        }
+
+        private static void Balance(int[] array, int index)
+        {
+            if (array.Length == 0)
+                return;
+
+            int centerIndex = array.Length / 2;
+
+            if (index < BBST.Length)
+                BBST[index] = array[centerIndex];
+
+            int[] leftArrWithoutMid = new int[centerIndex];
+            int[] rightArrWithoutMid = new int[array.Length - leftArrWithoutMid.Length - 1];
+
+            Array.Copy(array, 0, leftArrWithoutMid, 0, leftArrWithoutMid.Length);
+            Array.Copy(array, centerIndex + 1, rightArrWithoutMid, 0, rightArrWithoutMid.Length);
+
+            Balance(leftArrWithoutMid, 2 * index + 1);
+            Balance(rightArrWithoutMid, 2 * index + 2);
+        }
+    }
+}
---
+++ TestAlgoDataStructures/UnitTest3 (копия).cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures2;
+
+namespace Tests
+{
+    [TestClass]
+    public class BalancedBSTTests
+    {
+        [TestMethod]
+        public void GenerateBBSTArrayReturnsBSTArray()
+        {
+            int[] unsortedArray = { 5, 3, 6, 1, 4, 7, 2 };
+            int[] expectedResult = { 4, 2, 6, 1, 3, 5, 7 };
+
+            int[] result = BalancedBST.GenerateBBSTArray(unsortedArray);
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
